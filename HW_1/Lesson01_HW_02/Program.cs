72a80fdd4a6a4741469789b0f6c12373407fc849
Malakhov Alex <malakhovspb@gmail.com>
2018-02-08 10:14:04 +0300

    Add files via upload

---
+++ HW_1/Lesson01_HW_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Lesson01_HW_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int ;
+            double l, m, h;
+            string buff;
+            Console.WriteLine("Введите вес испытуемого в кг:");
+            buff = Console.ReadLine();
+            m = Convert.ToDouble(buff);
+            Console.WriteLine("Введите рост испытуемого в см:");
+            buff = Console.ReadLine();
+            h = Convert.ToDouble(buff);
+            l = m / (h * h);
+            Console.WriteLine("Индекс массы тела испытуемого равен:{0}", l);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW_1/Lesson01_HW_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01_HW_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2, r;
+           // int r;
+            string buff;
+            Console.WriteLine("Введите значения координат для x1 и y1");
+            buff = Console.ReadLine();
+            x1 = Convert.ToDouble(buff);
+            buff = Console.ReadLine();
+            y1 = Convert.ToDouble(buff);
+            Console.WriteLine("Введите значения координат для x2 и y2");
+            buff = Console.ReadLine();
+            x2 = Convert.ToDouble(buff);
+            buff = Console.ReadLine();
+            y2 = Convert.ToDouble(buff);
+            r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Расстояние между точками координат равняется: {0:F4}", r);
+            Console.ReadLine();
+            
+        }
+    }
+}
